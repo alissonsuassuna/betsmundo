40015dbde3bc51d6b2f895a547dd121d2bffb99d
Alisson Suassuna <alissonsuassuna@gmail.com>
2017-01-09 18:05:01 -0300

    Primeira tela finalizada

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the LoginPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
-  selector: 'page-login-page',
+  selector: 'login-page',
   templateUrl: 'login-page.html'
 })
 export class LoginPage {
